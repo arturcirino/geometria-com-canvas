0b0348ada187ac3ce8e26fceb86fa83be2ee0adf
Artur Cirino <artur.cirino@gmail.com>
2023-05-10 09:48:57 -0300

    Adicionando clearRect

--- quadrado/quadrado-com-rect.js
+++ quadrado/quadrado-com-rect.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.rect(30, 30, 100, 100);
-ctx.lineWidth = 5;
-ctx.stroke();
 ctx.fillStyle = "red";
 ctx.fill();
+ctx.clearRect(45, 45, 70, 70);
